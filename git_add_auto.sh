2874c84e18855e1a3f64da90e77643ccd8dbf8da
DandelionCN <25096281+DandelionCN@users.noreply.github.com>
2019-11-03 01:48:50 +0800

    修改自动提交版本的gitadd脚本.

--- git_add_auto.sh
+++ git_add_auto.sh
@@ -2,7 +2,7 @@
 git add .
 echo "please input the commit information:"
 read commit_info
-echo "the commit update information is:$commit_info"
+echo -e "\e[31;40m The commited update information is:$commit_info \e[0m"
 git commit -m "$commit_info"
 git push origin master
-echo "Update is commited successfully!"
+echo -e "\e[31;40m Update is commited successfully! \e[0m"
